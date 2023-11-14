7cc419af8dba1b87a70bec4765ee47c148d533ab
Mansoor Syed <mansoorhu10@gmail.com>
2023-11-14 00:39:01 -0500

    Spring Initializr Setup

---
+++ src/main/java/com/mansoorsyed/disasterassistant/DisasterassistantApplication.java
@@ -0,0 +1,13 @@
+package com.mansoorsyed.disasterassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterassistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisasterassistantApplication.class, args);
+	}
+
+}
