972d915e7b697bb3e1254d0f494a4edc235f5842
Mansoor Syed <mansoorhu10@gmail.com>
2023-11-30 15:21:40 -0500

    Added user class

---
+++ src/main/java/com/mansoorsyed/disasterassistant/model/user/User.java
@@ -0,0 +1,50 @@
+package com.mansoorsyed.disasterassistant.model.user;
+
+public class User {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+}
